4f9d4aca92cedaaa870fa73401b2e94e34f66154
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-11-07 11:10:33 +0700

    ubah configurasi awal

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'form_validation', 'upload', 'database', 'login_user', 'login_pelanggan');
+$autoload['libraries'] = array('session', 'form_validation', 'upload', 'database');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('file', 'url', 'global', 'form', 'form', 'string');
+$autoload['helper'] = array('file', 'url', 'form', 'form', 'string');
 
 /*
 | -------------------------------------------------------------------
