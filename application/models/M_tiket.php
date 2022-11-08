020fac81494f0c25d7f25d1a66e8a2eb8e22d210
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-11-08 15:18:56 +0700

    membuat proses pengambilan data tiket

---
+++ application/models/M_tiket.php
@@ -0,0 +1,41 @@
+<?php
+
+
+defined('BASEPATH') or exit('No direct sctipt access allowed');
+
+class M_tiket extends CI_Model
+{
+
+    public function tiket()
+    {
+        $this->db->select('*');
+        $this->db->from('tiket');
+        $this->db->order_by('id_tiket', 'desc');
+        return $this->db->get()->result();
+    }
+    public function tiket_detail($id_tiket)
+    {
+        $this->db->select('*');
+        $this->db->from('tiket');
+        $this->db->where('id_tiket', $id_tiket);
+        // $this->db->order_by('id_tiket', 'desc');
+        return $this->db->get()->row();
+    }
+
+    public function add($data)
+    {
+        $this->db->insert('tiket', $data);
+    }
+
+    public function edit($data)
+    {
+        $this->db->where('id_tiket', $data['id_tiket']);
+        $this->db->update('tiket', $data);
+    }
+
+    public function hapus($data)
+    {
+        $this->db->where('id_tiket', $data['id_tiket']);
+        $this->db->delete('tiket', $data);
+    }
+}
