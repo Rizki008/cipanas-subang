b92214c3b38caee01791ee7744029492a47f0d7c
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-11-11 08:29:26 +0700

    menambahkan menu transaksi dan diskon

--- application/views/backend/v_header.php
+++ application/views/backend/v_header.php
@@ -28,16 +28,16 @@
                         </div>
                     </div>
                     <a href="<?= base_url('tiket') ?>" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Tiket</a>
-                    <a href="form.html" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
-                    <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a>
-                    <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
+                    <a href="<?= base_url('user') ?>" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>User</a>
+                    <a href="<?= base_url('promo') ?>" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Promo</a>
+                    <!-- <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a> -->
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Pages</a>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Pemesanan</a>
                         <div class="dropdown-menu bg-transparent border-0">
-                            <a href="signin.html" class="dropdown-item">Sign In</a>
-                            <a href="signup.html" class="dropdown-item">Sign Up</a>
-                            <a href="404.html" class="dropdown-item">404 Error</a>
-                            <a href="blank.html" class="dropdown-item">Blank Page</a>
+                            <a href="signin.html" class="dropdown-item">Booking Masuk</a>
+                            <a href="signup.html" class="dropdown-item">Booking Proses</a>
+                            <a href="404.html" class="dropdown-item">Booking Selesai</a>
+                            <a href="blank.html" class="dropdown-item">Booking Batal</a>
                         </div>
                     </div>
                 </div>
