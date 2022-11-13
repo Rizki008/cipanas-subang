db5905b92d1c5920228c64c32d316f8afe3f37f1
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-11-13 14:48:08 +0700

    menambahka proses pemesanan tiket pada db

--- application/models/M_pemesanan.php
+++ application/models/M_pemesanan.php
@@ -14,21 +14,43 @@ class M_pemesanan extends CI_Model
         $this->db->insert('detail_pemesanan', $data_rinci);
     }
 
+    public function simpan_pembayaran($data)
+    {
+        $this->db->insert('pembayaran', $data);
+    }
 
     public function upload_buktibayar($data)
+    {
+        $this->db->where('id_pembayaran', $data['id_pembayaran']);
+        $this->db->update('pembayaran', $data);
+    }
+    public function update_status_pembayaran($data)
     {
         $this->db->where('id_pemesanan', $data['id_pemesanan']);
         $this->db->update('pemesanan', $data);
     }
 
     //informasi pesanan
-    public function belum_bayar()
+    public function pesanan()
     {
         $this->db->select('*');
         $this->db->from('pemesanan');
         $this->db->where('id_user', $this->session->userdata('id_user'));
-        $this->db->where('status_pemesanan=0');
+        // $this->db->where('status_pemesanan=0');
         $this->db->order_by('id_pemesanan', 'desc');
+        return $this->db->get()->result();
+    }
+    public function belum_bayar()
+    {
+        $this->db->select('*');
+        $this->db->from('pemesanan');
+        $this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
+        $this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
+        $this->db->where('id_user', $this->session->userdata('id_user'));
+        $this->db->where('status_pemesanan=0');
+        $this->db->order_by('pemesanan.id_pemesanan', 'desc');
+        $this->db->group_by('detail_pemesanan.id_pemesanan');
+
         return $this->db->get()->result();
     }
 
@@ -36,9 +58,12 @@ class M_pemesanan extends CI_Model
     {
         $this->db->select('*');
         $this->db->from('pemesanan');
+        $this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
+        $this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
         $this->db->where('id_user', $this->session->userdata('id_user'));
         $this->db->where('status_pemesanan=1');
-        $this->db->order_by('id_pemesanan', 'desc');
+        $this->db->order_by('pemesanan.id_pemesanan', 'desc');
+        $this->db->group_by('detail_pemesanan.id_pemesanan');
         return $this->db->get()->result();
     }
 
@@ -46,9 +71,12 @@ class M_pemesanan extends CI_Model
     {
         $this->db->select('*');
         $this->db->from('pemesanan');
+        $this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
+        $this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
         $this->db->where('id_user', $this->session->userdata('id_user'));
         $this->db->where('status_pemesanan=2');
-        $this->db->order_by('id_pemesanan', 'desc');
+        $this->db->order_by('pemesanan.id_pemesanan', 'desc');
+        $this->db->group_by('detail_pemesanan.id_pemesanan');
         return $this->db->get()->result();
     }
 
@@ -56,9 +84,12 @@ class M_pemesanan extends CI_Model
     {
         $this->db->select('*');
         $this->db->from('pemesanan');
+        $this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
+        $this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
         $this->db->where('id_user', $this->session->userdata('id_user'));
         $this->db->where('status_pemesanan=3');
-        $this->db->order_by('id_pemesanan', 'desc');
+        $this->db->order_by('pemesanan.id_pemesanan', 'desc');
+        $this->db->group_by('detail_pemesanan.id_pemesanan');
         return $this->db->get()->result();
     }
 
