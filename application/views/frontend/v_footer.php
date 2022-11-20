fac61260552bf20c4fd83419ed1f6a4c4e059f1f
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-11-21 00:12:28 +0700

    meperbaiki tampilan view wisatawan

--- application/views/frontend/v_footer.php
+++ application/views/frontend/v_footer.php
@@ -1,8 +1,8 @@
 <!--================ start footer Area  =================-->
 <footer class="footer-area section_gap">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-3  col-md-6 col-sm-6">
+        <!-- <div class="row"> -->
+        <!-- <div class="col-lg-3  col-md-6 col-sm-6">
                 <div class="single-footer-widget">
                     <h6 class="footer_title">About Agency</h6>
                     <p>The world has become so fast paced that people don’t want to stand by reading a page of information, they would much rather look at a presentation and understand the message. It has come to a point </p>
@@ -45,8 +45,8 @@
                         </form>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
+            </div> -->
+        <!-- <div class="col-lg-3 col-md-6 col-sm-6">
                 <div class="single-footer-widget instafeed">
                     <h6 class="footer_title">InstaFeed</h6>
                     <ul class="list_style instafeed d-flex flex-wrap">
@@ -60,15 +60,15 @@
                         <li><img src="<?= base_url() ?>royal-master/image/instagram/Image-08.jpg" alt=""></li>
                     </ul>
                 </div>
-            </div>
-        </div>
+            </div> -->
+        <!-- </div> -->
         <div class="border_line"></div>
         <div class="row footer-bottom d-flex justify-content-between align-items-center">
             <p class="col-lg-8 col-sm-12 footer-text m-0">
                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 Copyright &copy;<script>
                     document.write(new Date().getFullYear());
-                </script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                </script> Cipanas Subang Jawabarat <i class="fa fa-heart-o" aria-hidden="true"></i> <a href="https://colorlib.com" target="_blank">Kuningan Jawabarat</a>
                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             </p>
             <div class="col-lg-4 col-sm-12 footer-social">
