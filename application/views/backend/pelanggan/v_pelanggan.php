2043ea603452dc5e6f6cec2a1f9840febe1a04c8
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-12-14 10:41:09 +0700

    menambahkan view data wisatawan

---
+++ application/views/backend/pelanggan/v_pelanggan.php
@@ -0,0 +1,36 @@
+<div class="container-fluid pt-4 px-4">
+    <div class="row g-4">
+        <div class="col-sm-12 col-xl-12">
+            <div class="bg-light rounded h-100 p-4">
+                <h6 class="mb-4">Data Wisatawan</h6>
+                <!-- <a href="<?= base_url('tiket/add') ?>" class="btn btn-primary"> <i class="fa fa-plus"></i>Tambah Tiket</a> -->
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nama Wisatawan</th>
+                            <th scope="col">Jenis Kelamin</th>
+                            <th scope="col">Tempat, Tanggal Lahir</th>
+                            <th scope="col">No Hp</th>
+                            <th scope="col">Alamat</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $no = 1;
+                        foreach ($pelanggan as $key => $value) { ?>
+                            <tr>
+                                <th scope="row"><?= $no++ ?></th>
+                                <td><?= $value->nama_wisatawan ?></td>
+                                <td><?= $value->jk ?></td>
+                                <td><?= $value->ttl ?></td>
+                                <td><?= $value->no_hp ?></td>
+                                <td><?= $value->alamat_detail ?> <br> kab.<?= $value->kab_kota ?> Prov.<?= $value->provinsi ?></td>
+
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
