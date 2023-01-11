3f5b3b859aea3518e5010989324d1785facf4078
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-01-11 14:24:12 +0700

    menambahkan detail pesanan

--- application/models/M_pemesanan.php
+++ application/models/M_pemesanan.php
@@ -4,116 +4,124 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class M_pemesanan extends CI_Model
 {
-    public function simpan_pemesanan($data)
-    {
-        $this->db->insert('pemesanan', $data);
-    }
+	public function simpan_pemesanan($data)
+	{
+		$this->db->insert('pemesanan', $data);
+	}
 
-    public function simpan_detail_pemesanan($data_rinci)
-    {
-        $this->db->insert('detail_pemesanan', $data_rinci);
-    }
+	public function simpan_detail_pemesanan($data_rinci)
+	{
+		$this->db->insert('detail_pemesanan', $data_rinci);
+	}
 
-    public function simpan_pembayaran($data)
-    {
-        $this->db->insert('pembayaran', $data);
-    }
-    public function penilaian($data)
-    {
-        $this->db->insert('ulasan', $data);
-    }
+	public function simpan_pembayaran($data)
+	{
+		$this->db->insert('pembayaran', $data);
+	}
+	public function penilaian($data)
+	{
+		$this->db->insert('ulasan', $data);
+	}
 
-    public function upload_buktibayar($data)
-    {
-        $this->db->where('id_pembayaran', $data['id_pembayaran']);
-        $this->db->update('pembayaran', $data);
-    }
-    public function update_status_pembayaran($data)
-    {
-        $this->db->where('id_pemesanan', $data['id_pemesanan']);
-        $this->db->update('pemesanan', $data);
-    }
+	public function upload_buktibayar($data)
+	{
+		$this->db->where('id_pembayaran', $data['id_pembayaran']);
+		$this->db->update('pembayaran', $data);
+	}
+	public function update_status_pembayaran($data)
+	{
+		$this->db->where('id_pemesanan', $data['id_pemesanan']);
+		$this->db->update('pemesanan', $data);
+	}
 
-    //informasi pesanan
-    public function pesanan()
-    {
-        $this->db->select('*');
-        $this->db->from('pemesanan');
-        $this->db->where('id_wisatawan', $this->session->userdata('id_wisatawan'));
-        // $this->db->where('status_pemesanan=0');
-        $this->db->order_by('id_pemesanan', 'desc');
-        return $this->db->get()->result();
-    }
-    public function belum_bayar()
-    {
-        $this->db->select('*');
-        $this->db->from('pemesanan');
-        $this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
-        $this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
-        $this->db->where('id_wisatawan', $this->session->userdata('id_wisatawan'));
-        $this->db->where('status_pemesanan=0');
-        $this->db->order_by('pemesanan.id_pemesanan', 'desc');
-        $this->db->group_by('detail_pemesanan.id_pemesanan');
+	//informasi pesanan
+	public function pesanan()
+	{
+		$this->db->select('*');
+		$this->db->from('pemesanan');
+		$this->db->where('id_wisatawan', $this->session->userdata('id_wisatawan'));
+		// $this->db->where('status_pemesanan=0');
+		$this->db->order_by('id_pemesanan', 'desc');
+		return $this->db->get()->result();
+	}
+	public function belum_bayar()
+	{
+		$this->db->select('*');
+		$this->db->from('pemesanan');
+		$this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
+		$this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
+		$this->db->where('id_wisatawan', $this->session->userdata('id_wisatawan'));
+		$this->db->where('status_pemesanan=0');
+		$this->db->order_by('pemesanan.id_pemesanan', 'desc');
+		$this->db->group_by('detail_pemesanan.id_pemesanan');
 
-        return $this->db->get()->result();
-    }
+		return $this->db->get()->result();
+	}
 
-    public function diproses()
-    {
-        $this->db->select('*');
-        $this->db->from('pemesanan');
-        $this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
-        $this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
-        $this->db->where('id_wisatawan', $this->session->userdata('id_wisatawan'));
-        $this->db->where('status_pemesanan=1');
-        $this->db->order_by('pemesanan.id_pemesanan', 'desc');
-        $this->db->group_by('detail_pemesanan.id_pemesanan');
-        return $this->db->get()->result();
-    }
+	public function diproses()
+	{
+		$this->db->select('*');
+		$this->db->from('pemesanan');
+		$this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
+		$this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
+		$this->db->where('id_wisatawan', $this->session->userdata('id_wisatawan'));
+		$this->db->where('status_pemesanan=1');
+		$this->db->order_by('pemesanan.id_pemesanan', 'desc');
+		$this->db->group_by('detail_pemesanan.id_pemesanan');
+		return $this->db->get()->result();
+	}
 
-    public function selesai()
-    {
-        $this->db->select('*');
-        $this->db->from('pemesanan');
-        $this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
-        $this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
-        $this->db->where('id_wisatawan', $this->session->userdata('id_wisatawan'));
-        $this->db->where('status_pemesanan=2');
-        $this->db->order_by('pemesanan.id_pemesanan', 'desc');
-        $this->db->group_by('detail_pemesanan.id_pemesanan');
-        return $this->db->get()->result();
-    }
+	public function selesai()
+	{
+		$this->db->select('*');
+		$this->db->from('pemesanan');
+		$this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
+		$this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
+		$this->db->where('id_wisatawan', $this->session->userdata('id_wisatawan'));
+		$this->db->where('status_pemesanan=2');
+		$this->db->order_by('pemesanan.id_pemesanan', 'desc');
+		$this->db->group_by('detail_pemesanan.id_pemesanan');
+		return $this->db->get()->result();
+	}
 
-    public function batal()
-    {
-        $this->db->select('*');
-        $this->db->from('pemesanan');
-        $this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
-        $this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
-        $this->db->where('id_wisatawan', $this->session->userdata('id_wisatawan'));
-        $this->db->where('status_pemesanan=3');
-        $this->db->order_by('pemesanan.id_pemesanan', 'desc');
-        $this->db->group_by('detail_pemesanan.id_pemesanan');
-        return $this->db->get()->result();
-    }
+	public function batal()
+	{
+		$this->db->select('*');
+		$this->db->from('pemesanan');
+		$this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
+		$this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
+		$this->db->where('id_wisatawan', $this->session->userdata('id_wisatawan'));
+		$this->db->where('status_pemesanan=3');
+		$this->db->order_by('pemesanan.id_pemesanan', 'desc');
+		$this->db->group_by('detail_pemesanan.id_pemesanan');
+		return $this->db->get()->result();
+	}
 
-    public function detail($id_pemesanan)
-    {
-        $this->db->select('*');
-        $this->db->from('pemesanan');
-        $this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
-        $this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
-        $this->db->where('pemesanan.id_pemesanan', $id_pemesanan);
-        return $this->db->get()->result();
-    }
-    public function detail_rating($id_pemesanan)
-    {
-        $this->db->select('*');
-        $this->db->from('pemesanan');
-        $this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
-        $this->db->join('ulasan', 'detail_pemesanan.id_detail_pemesanan = ulasan.id_detail_pemesanan', 'left');
-        $this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
-        $this->db->where('pemesanan.id_pemesanan', $id_pemesanan);
-        return $this->db->get()->result();
-    }
+	public function detail_pesanan($id_pemesanan)
+	{
+		$this->db->select('*');
+		$this->db->from('pemesanan');
+		$this->db->where('id_pemesanan', $id_pemesanan);
+		return $this->db->get()->row();
+	}
+
+	public function detail($id_pemesanan)
+	{
+		$this->db->select('*');
+		$this->db->from('pemesanan');
+		$this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
+		$this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
+		$this->db->where('pemesanan.id_pemesanan', $id_pemesanan);
+		return $this->db->get()->result();
+	}
+	public function detail_rating($id_pemesanan)
+	{
+		$this->db->select('*');
+		$this->db->from('pemesanan');
+		$this->db->join('detail_pemesanan', 'pemesanan.id_pemesanan = detail_pemesanan.id_pemesanan', 'left');
+		$this->db->join('ulasan', 'detail_pemesanan.id_detail_pemesanan = ulasan.id_detail_pemesanan', 'left');
+		$this->db->join('tiket', 'detail_pemesanan.id_tiket = tiket.id_tiket', 'left');
+		$this->db->where('pemesanan.id_pemesanan', $id_pemesanan);
+		return $this->db->get()->result();
+	}
 }
--- application/views/frontend/pesanan/v_bayar.php
+++ application/views/frontend/pesanan/v_bayar.php
@@ -1,72 +1,74 @@
 <!--================Breadcrumb Area =================-->
 <section class="breadcrumb_area">
-    <div class="overlay bg-parallax" data-stellar-ratio="0.8" data-stellar-vertical-offset="0" data-background=""></div>
-    <div class="container">
-        <div class="page-cover text-center">
-            <h2 class="page-cover-tittle"><?= $title ?></h2>
-            <ol class="breadcrumb">
-                <li><a href="<?= base_url() ?>">Beranda</a></li>
-                <li class="active"><?= $title ?></li>
-            </ol>
-        </div>
-    </div>
+	<div class="overlay bg-parallax" data-stellar-ratio="0.8" data-stellar-vertical-offset="0" data-background=""></div>
+	<div class="container">
+		<div class="page-cover text-center">
+			<h2 class="page-cover-tittle"><?= $title ?></h2>
+			<ol class="breadcrumb">
+				<li><a href="<?= base_url() ?>">Beranda</a></li>
+				<li class="active"><?= $title ?></li>
+			</ol>
+		</div>
+	</div>
 </section>
 <!--================Breadcrumb Area =================-->
 
 <!--================Contact Area =================-->
 <section class="contact_area section_gap">
-    <div class="container">
-        <!-- <div id="mapBox" class="mapBox" data-lat="40.701083" data-lon="-74.1522848" data-zoom="13" data-info="PO Box CT16122 Collins Street West, Victoria 8007, Australia." data-mlat="40.701083" data-mlon="-74.1522848">
+	<div class="container">
+		<!-- <div id="mapBox" class="mapBox" data-lat="40.701083" data-lon="-74.1522848" data-zoom="13" data-info="PO Box CT16122 Collins Street West, Victoria 8007, Australia." data-mlat="40.701083" data-mlon="-74.1522848">
         </div> -->
-        <div class="row">
-            <div class="col-md-3">
-                <div class="contact_info">
-                    <div class="info_item">
-                        <i class="lnr lnr-home"></i>
-                        <h6>Cipanas Subang</h6>
-                        <p>Kuningan Jawa Barat</p>
-                    </div>
-                    <div class="info_item">
-                        <i class="lnr lnr-phone-handset"></i>
-                        <h6><a href="#">)892-1020-1029</a></h6>
-                        <p>Buka Senin-Minggu. Jam 08:00-16:00</p>
-                    </div>
-                    <div class="info_item">
-                        <i class="lnr lnr-envelope"></i>
-                        <h6><a href="#">cipanas_subang@gmail.com</a></h6>
-                        <!-- <p>Send us your query anytime!</p> -->
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-9">
-                <?php
-                //notifikasi form kosong
-                echo validation_errors('<div class="alert alert-warning alert-dismissible">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="contact_info">
+					<div class="info_item">
+						<i class="lnr lnr-home"></i>
+						<h6>Cipanas Subang</h6>
+						<p>Kuningan Jawa Barat</p>
+					</div>
+					<div class="info_item">
+						<i class="lnr lnr-phone-handset"></i>
+						<h6><a href="#">)892-1020-1029</a></h6>
+						<p>Buka Senin-Minggu. Jam 08:00-16:00</p>
+					</div>
+					<div class="info_item">
+						<i class="lnr lnr-envelope"></i>
+						<h6><a href="#">cipanas_subang@gmail.com</a></h6>
+						<!-- <p>Send us your query anytime!</p> -->
+					</div>
+				</div>
+			</div>
+			<div class="col-md-9">
+				<?php
+				//notifikasi form kosong
+				echo validation_errors('<div class="alert alert-warning alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             <h5><i class="icon fas fa-exclamation-triangle"></i>', '</h5></div>');
 
-                //notifikasi gagal upload gambar
-                if (isset($error_upload)) {
-                    echo '<div class="alert alert-warning alert-dismissible">
+				//notifikasi gagal upload gambar
+				if (isset($error_upload)) {
+					echo '<div class="alert alert-warning alert-dismissible">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                 <h5><i class="icon fas fa-exclamation-triangle"></i>' . $error_upload . '</h5></div>';
-                }
-                echo form_open_multipart('pemesanan/bayar/' . $pesanan->id_pemesanan) ?>
-                <!-- <form class="row contact_form" action="<?= base_url('pembelian/cekout') ?>" method="post" id="contactForm" novalidate="novalidate"> -->
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <input class="form-control" name="atas_nama" type="text" />
-                    </div><br>
-                    <div class="form-group">
-                        <input class="form-control" name="bukti_bayar" type="file" />
-                    </div><br><br><br>
-                </div>
-                <div class="col-md-12 text-right">
-                    <button type="submit" value="submit" class="btn theme_btn button_hover">Pesan Tiket</button>
-                </div>
-                </form>
-            </div>
-        </div>
-    </div>
+				}
+				echo form_open_multipart('pemesanan/bayar/' . $pesanan->id_pemesanan) ?>
+				<!-- <form class="row contact_form" action="<?= base_url('pembelian/cekout') ?>" method="post" id="contactForm" novalidate="novalidate"> -->
+				<div class="col-md-6">
+					<div class="form-group">
+						<h5>Atas Nama</h5>
+						<input class="form-control" name="atas_nama" type="text" />
+					</div><br>
+					<div class="form-group">
+						<h5>Bukti Pembayaran</h5>
+						<input class="form-control" name="bukti_bayar" type="file" />
+					</div><br><br><br>
+				</div>
+				<div class="col-md-12 text-right">
+					<button type="submit" value="submit" class="btn theme_btn button_hover">Pesan Tiket</button>
+				</div>
+				</form>
+			</div>
+		</div>
+	</div>
 </section>
 <!--================Contact Area =================-->
