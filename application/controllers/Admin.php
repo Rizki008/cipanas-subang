6f5461dad6276f90a359f7e89b6e1926aeeb89bf
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-02-28 13:51:10 +0700

    menambahkan proses grafik kedatangan wisatawan

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -22,6 +22,7 @@ class Admin extends CI_Controller
 			'tot_wisatawan' => $this->m_transaksi->tot_wisatawan(),
 			'grafik' => $this->m_transaksi->grafik(),
 			'grafik_kelamin' => $this->m_transaksi->grafik_kelamin(),
+			'grafik_datang' => $this->m_transaksi->grafik_datang(),
 			'isi' => 'v_admin'
 		);
 		$this->load->view('backend/v_wrapper', $data, FALSE);
