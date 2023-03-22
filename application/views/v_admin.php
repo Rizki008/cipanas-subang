2f1d95109b20b31c540414491b2ba53b34e9d02e
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-03-22 08:03:49 +0700

    mengubah grafik jenis kelamin'

--- application/views/v_admin.php
+++ application/views/v_admin.php
@@ -163,7 +163,7 @@
 		<div class="col-sm-12 col-xl-6">
 			<div class="bg-light text-center rounded p-4">
 				<div class="d-flex align-items-center justify-content-between mb-4">
-					<h6 class="mb-0">Analisis Jenis Kelamin</h6>
+					<h6 class="mb-0">Analisis Kedatangan Wiasatawan Berdasarkan Jenis Kelamin</h6>
 					<!-- <a href="">Show All</a> -->
 				</div>
 				<?php
