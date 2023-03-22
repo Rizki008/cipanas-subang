7190f5f7afa23568cbb692172721e3b865fd3f93
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-03-22 08:05:10 +0700

    memperbaiki js

--- application/views/backend/v_footer.php
+++ application/views/backend/v_footer.php
@@ -1,18 +1,18 @@
 <!-- Footer Start -->
 <div class="container-fluid pt-4 px-4">
-    <div class="bg-light rounded-top p-4">
-        <div class="row">
-            <div class="col-12 col-sm-6 text-center text-sm-start">
-                &copy; <a href="#">Cipanas</a>, Subang.
-            </div>
-            <div class="col-12 col-sm-6 text-center text-sm-end">
-                <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                Subang<a href="https://htmlcodex.com">Selajambe</a>
-                </br>
-                Kuningan <a class="border-bottom" href="https://themewagon.com" target="_blank">Jawabarat</a>
-            </div>
-        </div>
-    </div>
+	<div class="bg-light rounded-top p-4">
+		<div class="row">
+			<div class="col-12 col-sm-6 text-center text-sm-start">
+				&copy; <a href="#">Cipanas</a>, Subang.
+			</div>
+			<div class="col-12 col-sm-6 text-center text-sm-end">
+				<!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+				Subang<a href="https://htmlcodex.com">Selajambe</a>
+				</br>
+				Kuningan <a class="border-bottom" href="https://themewagon.com" target="_blank">Jawabarat</a>
+			</div>
+		</div>
+	</div>
 </div>
 <!-- Footer End -->
 </div>
@@ -38,17 +38,17 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
 </script>
 <!-- Template Javascript -->
-<script src="js/main.js"></script>
+<script src="<?= base_url() ?>backend/js/main.js"></script>
 <script>
-    console.log = function() {}
-    $("#pesan_produk").on('change', function() {
+	console.log = function() {}
+	$("#pesan_produk").on('change', function() {
 
-        $(".name").html($(this).find(':selected').attr('data-name'));
-        $(".name").val($(this).find(':selected').attr('data-name'));
+		$(".name").html($(this).find(':selected').attr('data-name'));
+		$(".name").val($(this).find(':selected').attr('data-name'));
 
-        $(".price").html($(this).find(':selected').attr('data-price'));
-        $(".price").val($(this).find(':selected').attr('data-price'));
-    });
+		$(".price").html($(this).find(':selected').attr('data-price'));
+		$(".price").val($(this).find(':selected').attr('data-price'));
+	});
 </script>
 </body>
 
