a503941a64e05100c6fdb0fc9402ef1011b43933
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-03-22 08:12:24 +0700

    menambahkan informasi tiket keluarga

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,19 +5,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Home extends CI_Controller
 {
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('m_home');
-    }
-    public function index()
-    {
-        $data = array(
-            'title' => 'Cipanas Subang',
-            'tiket' => $this->m_home->tiket(),
-            'ulasan' => $this->m_home->ulasan(),
-            'isi' => 'v_home'
-        );
-        $this->load->view('frontend/v_wrapper', $data, FALSE);
-    }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_home');
+	}
+	public function index()
+	{
+		$data = array(
+			'title' => 'Cipanas Subang',
+			'tiket' => $this->m_home->tiket(),
+			'ulasan' => $this->m_home->ulasan(),
+			'isi' => 'v_home'
+		);
+		$this->load->view('frontend/v_wrapper', $data, FALSE);
+	}
 }
