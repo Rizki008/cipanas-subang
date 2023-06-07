690fd7400e51696d9e2ab29e1446ec6ca75f5c9e
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-06-07 22:56:11 +0700

    menambahkan detail tiket dan pencarian data tiket

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,4 +20,28 @@ class Home extends CI_Controller
 		);
 		$this->load->view('frontend/v_wrapper', $data, FALSE);
 	}
+
+	public function search()
+	{
+		$keyword = $this->input->get('keyword');
+		$data = array(
+			'keyword'    => $keyword,
+			'pencarian' => $this->m_home->ambil_data($keyword),
+			'isi'        => 'frontend/pencarian/v_pencarian'
+		);
+		$this->load->view('frontend/v_wrapper', $data, FALSE);
+	}
+
+	public function detail_produk($id_tiket = null)
+	{
+		$data = array(
+			'title' => 'Detail Produk',
+			'data' => $this->m_home->detail_produk($id_tiket),
+			'reviews' => $this->m_home->reviews($id_tiket),
+			'views' => $this->m_home->views($id_tiket),
+			'views_ulasan' => $this->m_home->views_ulasan($id_tiket),
+			'isi' => 'frontend/booking/v_detail'
+		);
+		$this->load->view('frontend/v_wrapper', $data, FALSE);
+	}
 }
