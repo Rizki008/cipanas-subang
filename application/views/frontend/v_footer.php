e8a6f2e6ded38aba3e0d14222d778f439ec22695
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-08-21 09:19:49 +0700

    menambahkan js datepicker disable tanggal yang sudah ke lewat

--- application/views/frontend/v_footer.php
+++ application/views/frontend/v_footer.php
@@ -1,8 +1,8 @@
 <!--================ start footer Area  =================-->
 <footer class="footer-area section_gap">
-    <div class="container">
-        <!-- <div class="row"> -->
-        <!-- <div class="col-lg-3  col-md-6 col-sm-6">
+	<div class="container">
+		<!-- <div class="row"> -->
+		<!-- <div class="col-lg-3  col-md-6 col-sm-6">
                 <div class="single-footer-widget">
                     <h6 class="footer_title">About Agency</h6>
                     <p>The world has become so fast paced that people don’t want to stand by reading a page of information, they would much rather look at a presentation and understand the message. It has come to a point </p>
@@ -46,7 +46,7 @@
                     </div>
                 </div>
             </div> -->
-        <!-- <div class="col-lg-3 col-md-6 col-sm-6">
+		<!-- <div class="col-lg-3 col-md-6 col-sm-6">
                 <div class="single-footer-widget instafeed">
                     <h6 class="footer_title">InstaFeed</h6>
                     <ul class="list_style instafeed d-flex flex-wrap">
@@ -61,24 +61,24 @@
                     </ul>
                 </div>
             </div> -->
-        <!-- </div> -->
-        <div class="border_line"></div>
-        <div class="row footer-bottom d-flex justify-content-between align-items-center">
-            <p class="col-lg-8 col-sm-12 footer-text m-0">
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                Copyright &copy;<script>
-                    document.write(new Date().getFullYear());
-                </script> Cipanas Subang Jawabarat <i class="fa fa-heart-o" aria-hidden="true"></i> <a href="https://colorlib.com" target="_blank">Kuningan Jawabarat</a>
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-            <div class="col-lg-4 col-sm-12 footer-social">
-                <a href="#"><i class="fa fa-facebook"></i></a>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-                <a href="#"><i class="fa fa-dribbble"></i></a>
-                <a href="#"><i class="fa fa-behance"></i></a>
-            </div>
-        </div>
-    </div>
+		<!-- </div> -->
+		<div class="border_line"></div>
+		<div class="row footer-bottom d-flex justify-content-between align-items-center">
+			<p class="col-lg-8 col-sm-12 footer-text m-0">
+				<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+				Copyright &copy;<script>
+					document.write(new Date().getFullYear());
+				</script> Cipanas Subang Jawabarat <i class="fa fa-heart-o" aria-hidden="true"></i> <a href="https://colorlib.com" target="_blank">Kuningan Jawabarat</a>
+				<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+			</p>
+			<div class="col-lg-4 col-sm-12 footer-social">
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-dribbble"></i></a>
+				<a href="#"><i class="fa fa-behance"></i></a>
+			</div>
+		</div>
+	</div>
 </footer>
 <!--================ End footer Area  =================-->
 
@@ -98,33 +98,58 @@
 <script src="<?= base_url() ?>royal-master/vendors/lightbox/simpleLightbox.min.js"></script>
 <script src="<?= base_url() ?>royal-master/js/custom.js"></script>
 
+<!-- datepicker  -->
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+
 <!-- rating -->
 <script type="text/javascript" src="<?= base_url('assets/rating/') ?>js/star-rating.js"></script>
 <script type="text/javascript" src="<?= base_url('assets/rating/') ?>js/bootstrap.js"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-        var $inp = $('.rating-input');
-        $inp.rating({
-            min: 0,
-            max: 5,
-            step: 1,
-            size: 'sm',
-            showClear: false
-        });
-    });
+	$(document).ready(function() {
+		var $inp = $('.rating-input');
+		$inp.rating({
+			min: 0,
+			max: 5,
+			step: 1,
+			size: 'sm',
+			showClear: false
+		});
+	});
 </script>
 
 <script>
-    console.log = function() {}
-    $("#tiket").on('change', function() {
+	console.log = function() {}
+	$("#tiket").on('change', function() {
 
-        $(".tipe_tiket").html($(this).find(':selected').attr('data-tipe_tiket'));
-        $(".tipe_tiket").val($(this).find(':selected').attr('data-tipe_tiket'));
+		$(".tipe_tiket").html($(this).find(':selected').attr('data-tipe_tiket'));
+		$(".tipe_tiket").val($(this).find(':selected').attr('data-tipe_tiket'));
 
-        $(".harga_tiket").html($(this).find(':selected').attr('data-harga'));
-        $(".harga_tiket").val($(this).find(':selected').attr('data-harga'));
+		$(".harga_tiket").html($(this).find(':selected').attr('data-harga'));
+		$(".harga_tiket").val($(this).find(':selected').attr('data-harga'));
 
-    });
+	});
+</script>
+
+<!-- datepicker -->
+<script language="javascript">
+	var today = new Date();
+	var dd = String(today.getDate()).padStart(2, '0');
+	var mm = String(today.getMonth() + 1).padStart(2, '0');
+	var yyyy = today.getFullYear();
+
+	today = yyyy + '-' + mm + '-' + dd;
+	$('#date_picker').attr('min', today);
+</script>
+
+<script>
+	var today = new Date();
+	var dd = String(today.getDate()).padStart(2, '0');
+	var mm = String(today.getMonth() + 1).padStart(2, '0');
+	var yyyy = today.getFullYear();
+
+	today = yyyy + '-' + mm + '-' + dd;
+	$('#date').attr('min', today);
 </script>
 
 </body>
